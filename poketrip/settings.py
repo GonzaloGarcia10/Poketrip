cb37afb08494baaf93407e6bb51b4225e256e36d
GonzaloGarcia10 <gonzalo.garcia@valendra.tech>
2026-04-30 10:50:39 +0200

    add logging email.

--- poketrip/settings.py
+++ poketrip/settings.py
@@ -154,3 +154,15 @@ OPENAI_API_KEY = config('OPENAI_API_KEY', default='')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'},
+    },
+    'loggers': {
+        'django': {'handlers': ['console'], 'level': 'INFO'},
+        'django.core.mail': {'handlers': ['console'], 'level': 'DEBUG', 'propagate': False},
+    },
+}
