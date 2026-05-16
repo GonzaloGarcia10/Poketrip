d5d17c65aa1a2566647145caaaf6921ef9eb61bc
GonzaloGarcia10 <gonzalo.garcia@valendra.tech>
2026-05-16 17:26:08 +0200

    seo: add meta tags, robots.txt, sitemap and Schema.org

--- poketrip/urls.py
+++ poketrip/urls.py
@@ -1,23 +1,8 @@
-"""
-URL configuration for poketrip project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -26,4 +11,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('trips/', include('trips.urls')),
     path('', views.home, name='home'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
